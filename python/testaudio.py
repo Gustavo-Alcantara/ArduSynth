7b80e633789242c491fdf2362c52f498f02570f9
Gustavo Alcantara <gustavo.v.alcantara@gmail.com>
2023-06-11 17:37:34 -0300

    Playing multiple keys at the same time

--- python/testaudio.py
+++ python/testaudio.py
@@ -8,19 +8,25 @@ fps = 60
 screen = pg.display.set_mode([800,640])
 pg.display.set_caption("Synthesizer")
 
+keys = ['a','s','d','f','g','h','j']
 files = ["C4","D4","E4","F4","G4","A4","B4"]
 notes = []
 
 for f in files:
-    notes.append(mixer.Sound(f'assets/{f}.wav'))
+    notes.append(mixer.Sound(f'python/assets/{f}.wav'))
 
 
 while True:
     timer.tick(fps)
     screen.fill('black')
     for event in pg.event.get():
+
         if event.type == pg.QUIT:
             break
+        elif event.type == pg.TEXTINPUT:
+            for i,k in enumerate(keys):
+                if k in event.text:
+                    notes[i].play(0,1000)
     pg.display.flip()
 
 pg.quit()
