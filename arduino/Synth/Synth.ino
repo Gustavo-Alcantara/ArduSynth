c17b477d28208bdfc86efb2efffe5a16e0116d78
Gustavo Alcantara <gustavo.v.alcantara@gmail.com>
2023-06-11 14:46:54 -0300

    Directories created

---
+++ arduino/Synth/Synth.ino
@@ -0,0 +1,11 @@
+char 
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("Hello Computer");
+  delay(1000);
+}
