58dda52b4c162563ed10fdf3f80c5236224ff40e
Gustavo Alcantara <gustavo.v.alcantara@gmail.com>
2023-06-11 14:53:02 -0300

    Test Serial arduino

--- arduino/Synth/Synth.ino
+++ arduino/Synth/Synth.ino
@@ -1,4 +1,5 @@
-char 
+char notes[] = "0000000";
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -6,6 +7,9 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println("Hello Computer");
-  delay(1000);
+  for(int i = 0; i<7; i++){
+    notes[i] = '1';
+    Serial.println(notes);
+    delay(500);
+  }
 }
