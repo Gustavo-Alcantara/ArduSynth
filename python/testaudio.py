4521259a2891f1731fc7b28908155dd6f0fa1bed
Gustavo Alcantara <gustavo.v.alcantara@gmail.com>
2023-06-11 17:23:47 -0300

    Assets added

--- python/testaudio.py
+++ python/testaudio.py
@@ -2,12 +2,18 @@ import pygame as pg
 from pygame import mixer
 
 pg.init()
-
 pg.mixer.set_num_channels(50)
 timer = pg.time.Clock()
 fps = 60
 screen = pg.display.set_mode([800,640])
 pg.display.set_caption("Synthesizer")
+
+files = ["C4","D4","E4","F4","G4","A4","B4"]
+notes = []
+
+for f in files:
+    notes.append(mixer.Sound(f'assets/{f}.wav'))
+
 
 while True:
     timer.tick(fps)
