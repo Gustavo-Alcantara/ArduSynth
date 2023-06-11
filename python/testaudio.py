8e45ca5617ded55fd9cb006bfdcf2aae16070268
Gustavo Alcantara <gustavo.v.alcantara@gmail.com>
2023-06-11 17:12:00 -0300

    Starting to work with pygame

--- python/testaudio.py
+++ python/testaudio.py
@@ -1,25 +1,20 @@
-import pyaudio as pa
-import numpy as np
-import wave
+import pygame as pg
+from pygame import mixer
 
+pg.init()
 
-p = pa.PyAudio()
+pg.mixer.set_num_channels(50)
+timer = pg.time.Clock()
+fps = 60
+screen = pg.display.set_mode([800,640])
+pg.display.set_caption("Synthesizer")
 
+while True:
+    timer.tick(fps)
+    screen.fill('black')
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            break
+    pg.display.flip()
 
-freq = 440
-duration = 1
-t = np.linspace(0, duration, 44100)
-note = np.sin(2*np.pi*freq*t)
-note = note.astype(np.float32).tobytes()
-
-
-stream = p.open(format=pa.paFloat32,
-                channels=1,
-                rate=44100,
-                frames_per_buffer=1024,
-                output=True,
-                )
-stream.write(note)
-stream.stop_stream()
-stream.close()
-p.terminate()
+pg.quit()
